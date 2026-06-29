d5ed4ab9158681da8323650c052881856b1d0094
SuspectXXX <y13262639708@163.com>
2026-06-29 20:51:58 +0800

    sequential struct
    顺序结构的学习，图中不包含分叉路经

--- sequential_struct.py
+++ sequential_struct.py
@@ -1,3 +1,8 @@
+'''
+langgraph入门demo
+类似于其他语言的hello world
+全部是顺序结构
+'''
 from typing import TypedDict
 from langgraph.graph import StateGraph, START, END
 
@@ -12,17 +17,22 @@ def say_hello(state: HelloState) -> dict:
     result = f"你好，{name}！欢迎学习langgraph"
     return {"greeting": result}
 
+def say_goodbye(state: HelloState) -> dict:
+    return {"greeting": state["greeting"] + "\n加油学习langgraph"}
+
 # 初始化工作流图
 workflow = StateGraph(HelloState)
 
 # 为图添加节点，第一个参数是节点名，第二个参数是节点函数
 workflow.add_node("hello", say_hello)
+workflow.add_node("goodbye", say_goodbye)
 
 # 为图增加边，第一个参数是边的起点，第二个参数是边的终点
 # START、END为langgraph中的内置的起点和终点，近做标记，不执行业务逻辑
-# 整体流向为 START -> hello -> END
+# 整体流向为 START -> hello -> goodbye -> END
 workflow.add_edge(START, "hello")
-workflow.add_edge("hello", END)
+workflow.add_edge("hello", "goodbye")
+workflow.add_edge("goodbye", END)
 
 # 编译图，生成可执行实例。使图从定义阶段跨入可执行阶段
 app = workflow.compile()
